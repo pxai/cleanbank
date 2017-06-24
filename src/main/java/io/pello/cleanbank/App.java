19a5ed2166efa3e9ab68c0344d568395fba69437
pxai <pello_altadill@cuatrovientos.org>
2017-06-24 09:48:05 +0200

    First step

---
+++ src/main/java/io/pello/cleanbank/App.java
@@ -0,0 +1,13 @@
+package io.pello.cleanbank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
